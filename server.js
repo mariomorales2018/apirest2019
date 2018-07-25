bfa5988c8374ad716408dcd155d0223c3bece5ac
mariomorales2018 <41304544+mariomorales2018@users.noreply.github.com>
2018-07-25 17:57:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ var router = require('./app/routes');
 mongoose.connect(databaseConfig.url);
 
  
-app.listen(process.env.PORT || 9090);
+app.listen(process.env.PORT || 3000);
 console.log("App listening on port 9090");
-//app.use(express.favicon());
-//app.use(express.logger('dev'));
+app.use(express.favicon());
+app.use(express.logger('dev'));
 
-//app.use(express.methodOverride());
+app.use(express.methodOverride());
 
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json({limit: '50mb'})); // Send JSON responses

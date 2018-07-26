b87e2262c4626e2d54276ddfd211f9b99cf0f18a
mariomorales2018 <41304544+mariomorales2018@users.noreply.github.com>
2018-07-25 19:28:43 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
    // 'url': 'mongodb://usac:usac@ds111618.mlab.com:11618/usac'  
-     'url': 'mongodb://<dbuser>:<dbpassword>@ds253831.mlab.com:53831/usac'  
+     'url': 'mongodb://usac:ocitocit123@ds253831.mlab.com:53831/usac'  
 }

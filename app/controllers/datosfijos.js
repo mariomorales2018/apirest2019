0f7d50527cb16cf1908b5123c780710aa5e2d470
mariomorales2018 <41304544+mariomorales2018@users.noreply.github.com>
2018-10-02 13:25:19 -0600

    Add files via upload

--- app/controllers/datosfijos.js
+++ app/controllers/datosfijos.js
@@ -6,7 +6,7 @@ var csv      = require('csv-express');
 var Evento = require('../models/eventos');
 
 var Participa = require('../models/participa');
-
+var Facplan = require('../models/unidadplan');
 
 var request = require('request');
   
@@ -31,119 +31,90 @@ var cleanName = function(str) {
 exports.getCombofijo = function(req, res, next){
        var sql='';
 
-if(req.params.id=='buses-nelson')  
-{   
-        request('http://190.143.151.236:8500/ws/databuses.cfm', function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                   var importedJSON = JSON.parse(body);
-                   res.json(importedJSON);
-                }
-              })
-
-              
-        
-   
-}
-else
-{
-
-if(req.params.id=='catalogo-tipo')  
-{   
-        res.json([{id:'DIAS FESTIVOS',nombre:'DIAS FESTIVOS'},{id:'RUTAS',nombre:'RUTAS'},{id:'MATERIAS',nombre:'MATERIAS'},{id:'PERIODOSSUM',nombre:'PERIODOSSUM'},{id:'UNIDADES',nombre:'UNIDADES'} 
-        ,{id:'TIPO UNIDADES',nombre:'TIPO UNIDADES'}]);
-   
-}
-else
-{
-       if(req.params.id=='suscriptor-disp')  
-       {   
-               res.json([{id:'RFID interno',nombre:'RFID interno'} ,{id:'DPI',nombre:'DPI'},{id:'RFID externo',nombre:'RFID externo'},{id:'OTRO dispositivo',nombre:'OTRO dispositivo'},{id:'Ninguno',nombre:'Ninguno'}]);
-          
-       }
-       else
-       {
-        if(req.params.id=='modulo-grupo')  
-        {   
-                res.json([{id:'USAC',nombre:'USAC'} ,{id:'BUSES',nombre:'BUSES'},{id:'TICKES',nombre:'TICKES'},{id:'TODOS',nombre:'TODOS'}]);
-           
-        }
-        else
-        {
-                if(req.params.id=='user-rol')  
-                {   
+       switch(req.params.id) {
+        case 'buses-nelson':
+                                request('http://190.143.151.236:8500/ws/databuses.cfm', function (error, response, body) {
+                                        if (!error && response.statusCode == 200) {
+                                        var importedJSON = JSON.parse(body);
+                                        res.json(importedJSON);
+                                        }
+                                })
+            break;
+        case 'catalogo-tipo':
+                        res.json([{id:'DIAS FESTIVOS',nombre:'DIAS FESTIVOS'},{id:'RUTAS',nombre:'RUTAS'},{id:'MATERIAS',nombre:'MATERIAS'},{id:'PERIODOSSUM',nombre:'PERIODOSSUM'},{id:'UNIDADES',nombre:'UNIDADES'} 
+                        ,{id:'TIPO UNIDADES',nombre:'TIPO UNIDADES'}]);
+            break;
+        case 'suscriptor-disp':
+                  res.json([{id:'RFID interno',nombre:'RFID interno'} ,{id:'DPI',nombre:'DPI'},{id:'RFID externo',nombre:'RFID externo'},{id:'OTRO dispositivo',nombre:'OTRO dispositivo'},{id:'Ninguno',nombre:'Ninguno'}]);
+         
+        break;
+        case 'modulo-grupo':
+                        res.json([{id:'USAC',nombre:'USAC'} ,{id:'BUSES',nombre:'BUSES'},{id:'TICKES',nombre:'TICKES'},{id:'TODOS',nombre:'TODOS'}]);
+        break;
+        case 'user-rol':
                         Perfil.find({unidad:req.params.id2},function(err, todos) {
                                 if (err){  res.send(err);  }
-                                 res.json(todos);
+                                res.json(todos);
                         });
-                     
-                }
-                else
-                {
+        break;
+        case 'excel-eventos':
+                           var filename   = "eventos.csv";
 
-                if(req.params.id=='excel-eventos')  
-                {   
-                        var filename   = "eventos.csv";
+                                Participa.find({},function(err, todos2) {
+                                        if (err){ res.send(err); }
+                                        
 
-                        Participa.find({},function(err, todos2) {
-                                if (err){ res.send(err); }
-                                
+                                        if(todos2.length>0)   {  
 
-                                if(todos2.length>0)   {  
+                                        //   res.json(todos2);
 
-                                     //   res.json(todos2);
-
-                                        Evento.find({impresion:'Activo'}).lean().exec({}, function(err,todos) {
-                                                if (err) res.send(err);
-                                                var myData = [];
-                                                var cc=0;
-                                                for(var i = 0; i < todos.length;i++){
-                                                       
-                                                        for(var j = 0; j < todos2.length;j++){
-                                                             
-                                                                if(todos[i]._id==todos2[j].idevento)
-                                                                {
-                                                                        cc=cc+1;
-                                                                }
-                                                                        
-                                                        }       
+                                                Evento.find({impresion:'Activo'}).lean().exec({}, function(err,todos) {
+                                                        if (err) res.send(err);
+                                                        var myData = [];
+                                                        var cc=0;
+                                                        for(var i = 0; i < todos.length;i++){
+                                                        
+                                                                for(var j = 0; j < todos2.length;j++){
+                                                                
+                                                                        if(todos[i]._id==todos2[j].idevento)
+                                                                        {
+                                                                                cc=cc+1;
+                                                                        }
+                                                                                
+                                                                }       
 
 
-                                                myData.push({nombre:cleanName(todos[i].nombre),Noparticipantes:cc});
-                                                cc=0;
-                                                }
+                                                        myData.push({nombre:cleanName(todos[i].nombre),Noparticipantes:cc});
+                                                        cc=0;
+                                                        }
+                                                        
+                                                        res.statusCode = 200;
+                                                        res.setHeader('Content-Type', 'text/csv; charset=utf-8');
+                                                        res.setHeader("Content-Disposition", 'attachment; filename='+filename);
+                                                        res.csv(myData, true);
+                                                //  res.json(todos2);  
+                                                });
+
                                                 
-                                                res.statusCode = 200;
-                                                res.setHeader('Content-Type', 'text/csv; charset=utf-8');
-                                                res.setHeader("Content-Disposition", 'attachment; filename='+filename);
-                                                res.csv(myData, true);
-                                              //  res.json(todos2);  
-                                        });
-
-                                         
-                                }
-                               
-                        });
+                                        }
+                                
+                                });
 
 
-                        
-                     
-                }
-                else
-                {
+        break;
+        case 'excel-participa':
 
-                        if(req.params.id=='excel-participa')  
-                        {   
                                 var filename   = "participantes.csv";
-        
+                                
                                 Participa.find({idevento:req.params.id2,estado:'Ingreso'}).sort({nombre:1}).exec(function(err, todos2) {
                                         if (err){ res.send(err); }
                                         
-        
+
                                         if(todos2.length>0)   {  
-        
+
                                                 var myData = [];
-                                                 for(var i = 0; i < todos2.length;i++){
-               
+                                                for(var i = 0; i < todos2.length;i++){
+
 
                                                 myData.push({nombre:cleanName(todos2[i].nombre) + ' '+ cleanName(todos2[i].apellido),fecha:todos2[i].fecha.substr(0,10),ingresos:cleanName(todos2[i].cuenta) });
                                                 }
@@ -152,83 +123,121 @@ else
                                                 res.setHeader('Content-Type', 'text/csv; charset=utf-8');
                                                 res.setHeader("Content-Disposition", 'attachment; filename='+filename);
                                                 res.csv(myData, true);
-                                             
-                                                 
+                                        
+                                                
                                         }
-                                       
-                                });
-        
-        
                                 
-                             
-                        }
-                        else
-                        {
+                                });
+
+        break;
+        case 'excel-asigna':
+
+                                var filename   = "asignaciones.csv";
+                                
+                                Facplan.find({'idperiodo.nombre':'2018-04'}).sort({'idunidadacademica.codigo':1}).exec(function(err, todos2) {
+                                        if (err){ res.send(err); }
+                                        
+
+                                        if(todos2.length>0)   {  
+
+                                                var myData = [];
+                                                for(var i = 0; i < todos2.length;i++){
+
+                                                        var d =new Date( todos2[i].fexamen).toISOString()
+                                                        var n = d.toString();      
+                                                        var ll=''
+                                                        if(todos2[i].idmateria=='Lenguaje'){ll='3'}
+                                                        if(todos2[i].idmateria=='Matematica'){ll='4'}
+                                                        if(todos2[i].idmateria=='Fisica'){ll='2'}
+                                                        if(todos2[i].idmateria=='Quimica'){ll='5'}
+                                                        if(todos2[i].idmateria=='Biologia'){ll='1'}
+
+                                                        
+                                                myData.push({periodo:cleanName(todos2[i].idperiodo.nombre),codigounidad:todos2[i].idunidadacademica.codigo,unidadacademica:todos2[i].idunidadacademica.nombre,edificio:cleanName(todos2[i].idedificio.nombre),salon:cleanName(todos2[i].idsalon.nombre)
+                                                        ,codigomateria:ll,materia:cleanName(todos2[i].idmateria),horario:cleanName(todos2[i].idhorario)
+                                                        ,capacidad:todos2[i].capacidad ,asignados:todos2[i].asignados
+                                                        ,fexamen:d.substr(0,10) ,codfac:todos2[i].codfac 
+                                                 });
+                                                }
+                                                
+                                                res.statusCode = 200;
+                                                res.setHeader('Content-Type', 'text/csv; charset=utf-8');
+                                                res.setHeader("Content-Disposition", 'attachment; filename='+filename);
+                                                res.csv(myData, true);
+                                        
+                                                
+                                        }
+                                
+                                });
+
+        break;
+  
+        default:
+
+        Perfil.find({nombre:req.params.id},function(err, todos) {
+                if (err){ res.send(err); }
+                
+               
+                if(todos.length>0)   {  
                         
-                        Perfil.find({nombre:req.params.id},function(err, todos) {
+                
+                        Permiso.find({idrol:todos[0]._id},function(err, todos) {
                                 if (err){ res.send(err); }
                                 
                                
                                 if(todos.length>0)   {  
                                         
-                                
-                                        Permiso.find({idrol:todos[0]._id},function(err, todos) {
+                                        Moduloxx.find({},function(err, todos2) {
                                                 if (err){ res.send(err); }
-                                                
                                                
-                                                if(todos.length>0)   {  
-                                                        
-                                                        Moduloxx.find({},function(err, todos2) {
-                                                                if (err){ res.send(err); }
-                                                               
-                                                                var myData = [];
-                                                                for(var i = 0; i < todos.length;i++){
-                                                                        for(var j = 0; j < todos2.length;j++){
-                                                                                if(todos[i].nombre==todos2[j].nombre)
-                                                                                {
-                                                                                    //    console.log(todos[i].nombre);
-                                                                                        myData.push({idrol:todos[i].idrol,title:todos2[j].nombre,component: todos2[j].componente, tabComponent:todos2[j].tabcomponente,name:todos2[j].titulo,index:todos2[j].index,icon:todos2[j].icono,estado:todos2[j].estado,
-                                                                                        permiso:todos[i].ingreso+','+todos[i].consulta+','+todos[i].eliminacion+','+todos[i].creacion+','+todos[i].actualizacion});
-                                                
-                                                                                        break;                
-                                                                                }
-
-
-                                                                        }          
-                                                                      
-                                                                       
+                                                var myData = [];
+                                                for(var i = 0; i < todos.length;i++){
+                                                        for(var j = 0; j < todos2.length;j++){
+                                                                if(todos[i].nombre==todos2[j].nombre)
+                                                                {
+                                                                    //    console.log(todos[i].nombre);
+                                                                        myData.push({idrol:todos[i].idrol,title:todos2[j].nombre,component: todos2[j].componente, tabComponent:todos2[j].tabcomponente,name:todos2[j].titulo,index:todos2[j].index,icon:todos2[j].icono,estado:todos2[j].estado,
+                                                                        permiso:todos[i].ingreso+','+todos[i].consulta+','+todos[i].eliminacion+','+todos[i].creacion+','+todos[i].actualizacion});
                                 
+                                                                        break;                
                                                                 }
-                                
-                                                                 res.json(myData);
-                                                        
 
 
-                                                        
-                                                    });       
-
+                                                        }          
+                                                      
                                                        
-                                                
+                
                                                 }
-                                                else
-                                                {  res.status(500).send('NO EXISTE ROL ASIGNADO');      }
-                                                
-                                            });
+                
+                                                 res.json(myData);
+                                        
 
-                                      
-                                
+
+                                        
+                                    });       
+
+                                       
                                 
                                 }
                                 else
                                 {  res.status(500).send('NO EXISTE ROL ASIGNADO');      }
                                 
                             });
-                        }
-                }}
-               
-            
-        }}
-}
-}
+
+                      
+                
+                
+                }
+                else
+                {  res.status(500).send('NO EXISTE ROL ASIGNADO');      }
+                
+            });
+
+
+    }
+
+
+
+
     
 }

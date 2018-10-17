6d0824307a361b4f04e446bad977e1549596f0b3
mariomorales2018 <41304544+mariomorales2018@users.noreply.github.com>
2018-10-17 15:23:44 -0600

    Add files via upload

--- app/models/permison2.js
+++ app/models/permison2.js
@@ -1,6 +1,7 @@
 var mongoose =require("mongoose");
 
-    var PermisoSchema 		=	new  mongoose.Schema({
+    var Permison2Schema 		=	new  mongoose.Schema({
+        idpermiso   		: { type : String, required : true },
         idrol   		: { type : String, required : true },
         nombre   		: { type : String, required : true },
         icon   		: { type : String },
@@ -16,5 +17,5 @@ var mongoose =require("mongoose");
 
 
 
-module.exports = mongoose.model('Permiso', PermisoSchema);
+module.exports = mongoose.model('Permison2', Permison2Schema);
 
